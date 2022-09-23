2f656c1b49c5f62420bc45e4587721da879d729d
Hua Yang <shinasaurus@gmail.com>
2022-09-23 01:39:51 -0700

    log4jbranch 2nd commit

---
+++ src/test/java/log4jagain/Log4jDemoAgain.java
@@ -0,0 +1,29 @@
+package log4jagain;
+
+import org.apache.logging.log4j.*;
+
+public class Log4jDemoAgain {
+	
+	private static Logger demoLogger = LogManager.getLogger(Log4jDemoAgain.class.getName());
+
+	public static void main(String[] args) {
+		
+		String str ="C:\\Shine\\automcode\\githhub-SeleniumFramework\\.\\logs\\Logdemo-01:12:17:159.log\n";
+		System.out.println(str.charAt(62));
+		
+		System.out.println("This is syso: " + demoLogger.getName());
+		demoLogger.info("Click successfull");
+		demoLogger.error("DB connection failed");
+		demoLogger.debug("This is debug");
+		demoLogger.fatal("This is fatal");
+		demoLogger.trace("This is trace");
+		demoLogger.info("Click successfull");
+		demoLogger.error("DB connection failed");
+		demoLogger.debug("This is debug");
+		demoLogger.fatal("This is fatal");
+		demoLogger.trace("This is trace");
+		
+
+	}
+
+}
